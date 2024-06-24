af8c2ae08230b8314f8268780f42e684c8e16d06
radhikaguptaa786 <radhikaguptaa786@gmail.com>
2024-06-24 17:30:04 +0530

    first commit

---
+++ client/src/resources/Themes.js
@@ -0,0 +1,73 @@
+import { abcdef } from "@uiw/codemirror-themes-all"
+import { abyss } from "@uiw/codemirror-themes-all"
+import { androidstudio } from "@uiw/codemirror-themes-all"
+import { andromeda } from "@uiw/codemirror-themes-all"
+import { atomone } from "@uiw/codemirror-themes-all"
+import { aura } from "@uiw/codemirror-themes-all"
+import { basicDark } from "@uiw/codemirror-themes-all"
+import { basicLight } from "@uiw/codemirror-themes-all"
+import { bbedit } from "@uiw/codemirror-themes-all"
+import { bespin } from "@uiw/codemirror-themes-all"
+import { copilot } from "@uiw/codemirror-themes-all"
+import { darcula } from "@uiw/codemirror-themes-all"
+import { dracula } from "@uiw/codemirror-themes-all"
+import { duotoneDark } from "@uiw/codemirror-themes-all"
+import { duotoneLight } from "@uiw/codemirror-themes-all"
+import { eclipse } from "@uiw/codemirror-themes-all"
+import { githubDark } from "@uiw/codemirror-themes-all"
+import { githubLight } from "@uiw/codemirror-themes-all"
+import { gruvboxDark } from "@uiw/codemirror-themes-all"
+import { gruvboxLight } from "@uiw/codemirror-themes-all"
+import { kimbie } from "@uiw/codemirror-themes-all"
+import { material } from "@uiw/codemirror-themes-all"
+import { materialDark } from "@uiw/codemirror-themes-all"
+import { materialLight } from "@uiw/codemirror-themes-all"
+import { monokai } from "@uiw/codemirror-themes-all"
+import { monokaiDimmed } from "@uiw/codemirror-themes-all"
+import { noctisLilac } from "@uiw/codemirror-themes-all"
+import { nord } from "@uiw/codemirror-themes-all"
+import { okaidia } from "@uiw/codemirror-themes-all"
+import { quietlight } from "@uiw/codemirror-themes-all"
+import { red } from "@uiw/codemirror-themes-all"
+import { solarizedDark } from "@uiw/codemirror-themes-all"
+import { solarizedLight } from "@uiw/codemirror-themes-all"
+import { sublime } from "@uiw/codemirror-themes-all"
+import { tokyoNight } from "@uiw/codemirror-themes-all"
+import { tokyoNightDay } from "@uiw/codemirror-themes-all"
+import { tokyoNightStorm } from "@uiw/codemirror-themes-all"
+import { tomorrowNightBlue } from "@uiw/codemirror-themes-all"
+import { vscodeDark } from "@uiw/codemirror-themes-all"
+import { whiteDark } from "@uiw/codemirror-themes-all"
+import { whiteLight } from "@uiw/codemirror-themes-all"
+import { xcodeDark } from "@uiw/codemirror-themes-all"
+import { xcodeLight } from "@uiw/codemirror-themes-all"
+
+import {} from "@uiw/codemirror-themes-all"
+
+export const editorThemes = {
+    Abcdef: abcdef,
+    Abyss: abyss,
+    "Android Studio": androidstudio,
+    Aura: aura,
+    "Basic Dark": basicDark,
+    "Basic Light": basicLight,
+    Copilot: copilot,
+    Darcula: darcula,
+    Eclipse: eclipse,
+    "GitHub Dark": githubDark,
+    "GitHub Light": githubLight,
+    Material: material,
+    "Material Dark": materialDark,
+    "Material Light": materialLight,
+    "Quiet Light": quietlight,
+    Sublime: sublime,
+    "Tokyo Night": tokyoNight,
+    "Tokyo Night Day": tokyoNightDay,
+    "Tokyo Night Storm": tokyoNightStorm,
+    "Tomorrow Night Blue": tomorrowNightBlue,
+    "VS Code Dark": vscodeDark,
+    "White Dark": whiteDark,
+    "White Light": whiteLight,
+    "Xcode Dark": xcodeDark,
+    "Xcode Light": xcodeLight,
+}
